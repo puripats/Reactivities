1dce81034b85f11af283d0c6037f0526081ad5c0
puripats <choroomz@gmail.com>
2020-05-28 01:35:43 +0700

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,14 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            
+            services.AddCors(opt=>
+            {
+                opt.AddPolicy("CorsPolicy", policy=>
+                {   
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -45,6 +53,8 @@ namespace API
             }
 
           //  app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
